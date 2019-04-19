9a60c18f18bd3ddb7f3d7abf204c9f92b5f4a77f
fredjust <fredjust@gmail.com>
2019-04-19 17:23:23 +0200

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,300 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frMain
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.chk_Send = New System.Windows.Forms.CheckBox()
+        Me.lblFile = New System.Windows.Forms.Label()
+        Me.bt_File = New System.Windows.Forms.Button()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txt_File = New System.Windows.Forms.TextBox()
+        Me.bt_save = New System.Windows.Forms.Button()
+        Me.txt_Password = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_UserName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_Server = New System.Windows.Forms.TextBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.txt_Second = New System.Windows.Forms.TextBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(508, 278)
+        Me.TabControl1.TabIndex = 2
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.TabPage1.Controls.Add(Me.txt_Second)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.chk_Send)
+        Me.TabPage1.Controls.Add(Me.lblFile)
+        Me.TabPage1.Controls.Add(Me.bt_File)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(500, 252)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "File"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(92, 37)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Seconds"
+        '
+        'chk_Send
+        '
+        Me.chk_Send.AutoSize = True
+        Me.chk_Send.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chk_Send.Location = New System.Drawing.Point(8, 61)
+        Me.chk_Send.Name = "chk_Send"
+        Me.chk_Send.Size = New System.Drawing.Size(209, 33)
+        Me.chk_Send.TabIndex = 5
+        Me.chk_Send.Text = "Check and Send"
+        Me.chk_Send.UseVisualStyleBackColor = True
+        '
+        'lblFile
+        '
+        Me.lblFile.AutoSize = True
+        Me.lblFile.Location = New System.Drawing.Point(92, 11)
+        Me.lblFile.Name = "lblFile"
+        Me.lblFile.Size = New System.Drawing.Size(49, 13)
+        Me.lblFile.TabIndex = 3
+        Me.lblFile.Text = "name file"
+        '
+        'bt_File
+        '
+        Me.bt_File.Location = New System.Drawing.Point(8, 6)
+        Me.bt_File.Name = "bt_File"
+        Me.bt_File.Size = New System.Drawing.Size(75, 23)
+        Me.bt_File.TabIndex = 2
+        Me.bt_File.Text = "File :"
+        Me.bt_File.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.TabPage2.Controls.Add(Me.Label5)
+        Me.TabPage2.Controls.Add(Me.txt_File)
+        Me.TabPage2.Controls.Add(Me.bt_save)
+        Me.TabPage2.Controls.Add(Me.txt_Password)
+        Me.TabPage2.Controls.Add(Me.Label3)
+        Me.TabPage2.Controls.Add(Me.txt_UserName)
+        Me.TabPage2.Controls.Add(Me.Label2)
+        Me.TabPage2.Controls.Add(Me.Label1)
+        Me.TabPage2.Controls.Add(Me.txt_Server)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(500, 252)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "FTP"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 37)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(23, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "File"
+        '
+        'txt_File
+        '
+        Me.txt_File.Location = New System.Drawing.Point(72, 34)
+        Me.txt_File.Name = "txt_File"
+        Me.txt_File.Size = New System.Drawing.Size(206, 20)
+        Me.txt_File.TabIndex = 7
+        '
+        'bt_save
+        '
+        Me.bt_save.Location = New System.Drawing.Point(203, 112)
+        Me.bt_save.Name = "bt_save"
+        Me.bt_save.Size = New System.Drawing.Size(75, 23)
+        Me.bt_save.TabIndex = 6
+        Me.bt_save.Text = "Save"
+        Me.bt_save.UseVisualStyleBackColor = True
+        '
+        'txt_Password
+        '
+        Me.txt_Password.Location = New System.Drawing.Point(72, 86)
+        Me.txt_Password.Name = "txt_Password"
+        Me.txt_Password.Size = New System.Drawing.Size(206, 20)
+        Me.txt_Password.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Password"
+        '
+        'txt_UserName
+        '
+        Me.txt_UserName.Location = New System.Drawing.Point(72, 60)
+        Me.txt_UserName.Name = "txt_UserName"
+        Me.txt_UserName.Size = New System.Drawing.Size(206, 20)
+        Me.txt_UserName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "User Name"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Server"
+        '
+        'txt_Server
+        '
+        Me.txt_Server.Location = New System.Drawing.Point(72, 7)
+        Me.txt_Server.Name = "txt_Server"
+        Me.txt_Server.Size = New System.Drawing.Size(206, 20)
+        Me.txt_Server.TabIndex = 0
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripProgressBar1, Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 256)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(508, 22)
+        Me.StatusStrip1.TabIndex = 5
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+        Me.ToolStripProgressBar1.Value = 50
+        Me.ToolStripProgressBar1.Visible = False
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(60, 17)
+        Me.ToolStripStatusLabel1.Text = "Waiting ..."
+        '
+        'txt_Second
+        '
+        Me.txt_Second.BackColor = System.Drawing.Color.Pink
+        Me.txt_Second.Location = New System.Drawing.Point(8, 34)
+        Me.txt_Second.Name = "txt_Second"
+        Me.txt_Second.Size = New System.Drawing.Size(75, 20)
+        Me.txt_Second.TabIndex = 8
+        Me.txt_Second.Text = "5"
+        Me.txt_Second.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'frMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(508, 278)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "frMain"
+        Me.Text = "Update FTP "
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents lblFile As System.Windows.Forms.Label
+    Friend WithEvents bt_File As System.Windows.Forms.Button
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents txt_Password As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txt_UserName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_Server As System.Windows.Forms.TextBox
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents chk_Send As System.Windows.Forms.CheckBox
+    Friend WithEvents bt_save As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txt_File As System.Windows.Forms.TextBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripProgressBar1 As System.Windows.Forms.ToolStripProgressBar
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents txt_Second As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,115 @@
+﻿
+Imports System.IO
+Imports System.Net
+
+
+
+
+Public Class frMain
+
+
+    Dim IniFile As String
+    Dim LastDate As Date
+
+
+    Private Sub SendFTP()
+        Try
+
+            Dim request As FtpWebRequest = DirectCast(WebRequest.Create(txt_Server.Text & "/" & txt_File.Text), System.Net.FtpWebRequest) 'On renseigne la futur destination du fichier à envoyer
+            request.Credentials = New NetworkCredential(txt_UserName.Text, txt_Password.Text) 'On rentre les identifiant et mot de passe
+            request.Method = System.Net.WebRequestMethods.Ftp.UploadFile 'On indique qu'on veut upload un fichier
+            Dim files() As Byte = File.ReadAllBytes(lblFile.Text) 'On indique le chemin du fichier à upload
+            Dim strz As Stream = request.GetRequestStream() 'On créer un stream qui va nous permettre d'envoyer le fichier
+            strz.Write(files, 0, files.Length) 'On envoie le fichier
+            strz.Close() 'On ferme la connection
+            strz.Dispose() 'On supprime la connection
+
+            ToolStripStatusLabel1.Text = "Last Send: " & Date.Now.ToString
+            ToolStripStatusLabel1.BackColor = Color.Transparent
+
+        Catch ex As Exception
+            ToolStripStatusLabel1.Text = "Error : " & ex.Message
+            ToolStripStatusLabel1.BackColor = Color.Red
+        End Try
+    End Sub
+
+    'Choose a file and save name in ini file
+    Private Sub bt_File_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt_File.Click
+        With OpenFileDialog1
+            .FileName = ""
+            .Filter = "All Files (*.*) | *.*"
+            If .ShowDialog = Windows.Forms.DialogResult.OK Then
+                lblFile.Text = .FileName
+                Cls_Ini.INIWrite(IniFile, "Local", "File", .FileName)
+            End If
+        End With
+    End Sub
+
+    Private Sub chk_Send_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chk_Send.CheckedChanged
+        ToolStripProgressBar1.Visible = chk_Send.Checked
+        Timer1.Enabled = chk_Send.Checked
+        If Timer1.Enabled Then
+            checkAndSend()
+        End If
+    End Sub
+
+    'load last option in ini file
+    Private Sub frMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+       
+        IniFile = Application.StartupPath & "\LastOption.ini"
+        If Cls_Ini.INISectionExist(IniFile, "FTP") Then
+            txt_Server.Text = Cls_Ini.INIRead(IniFile, "FTP", "Server")
+            txt_File.Text = Cls_Ini.INIRead(IniFile, "FTP", "File")
+            txt_UserName.Text = Cls_Ini.INIRead(IniFile, "FTP", "UserName")
+            txt_Password.Text = Cls_Ini.INIRead(IniFile, "FTP", "PassWord")
+        End If
+        If Cls_Ini.INISectionExist(IniFile, "Local") Then
+            lblFile.Text = Cls_Ini.INIRead(IniFile, "Local", "File")
+            txt_Second.Text = Cls_Ini.INIRead(IniFile, "Local", "Second")
+        End If
+
+    End Sub
+
+    'Save FTP option in ini file
+    Private Sub bt_save_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt_save.Click
+
+        Cls_Ini.INIWrite(IniFile, "FTP", "Server", txt_Server.Text)
+        Cls_Ini.INIWrite(IniFile, "FTP", "File", txt_File.Text)
+        Cls_Ini.INIWrite(IniFile, "FTP", "UserName", txt_UserName.Text)
+        Cls_Ini.INIWrite(IniFile, "FTP", "PassWord", txt_Password.Text)
+
+    End Sub
+
+
+
+    Private Sub checkAndSend()
+        If LastDate < File.GetLastWriteTime(lblFile.Text) Then
+            LastDate = File.GetLastWriteTime(lblFile.Text)
+            SendFTP()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        If ToolStripProgressBar1.Value = ToolStripProgressBar1.Maximum Then
+            ToolStripProgressBar1.Value = 0
+        End If
+
+        ToolStripProgressBar1.Value += 1
+
+        If ToolStripProgressBar1.Value = ToolStripProgressBar1.Maximum Then
+            checkAndSend()
+        End If
+    End Sub
+
+    Private Sub txt_second_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txt_Second.TextChanged
+        If Not IsNumeric(txt_Second.Text) Then
+            txt_Second.BackColor = Color.Pink
+        Else
+            txt_Second.BackColor = Color.GreenYellow
+            ToolStripProgressBar1.Maximum = txt_Second.Text
+            Cls_Ini.INIWrite(IniFile, "Local", "Second", txt_Second.Text)
+        End If
+
+    End Sub
+End Class
